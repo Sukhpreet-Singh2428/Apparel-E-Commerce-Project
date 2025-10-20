4ce3083b1df537305c1529533410d2b3c9c6f194
Sukhpreet Singh <sukhpreetsingh3910a@gmail.com>
2025-10-20 13:37:09 +0530

    making api for handle the users login and respond accordingly

---
+++ JS/loginpage.js
@@ -0,0 +1,24 @@
+const API_URL = "http://localhost:3000/users";
+const result = document.getElementById("result");
+
+document.getElementById("loginform").addEventListener("submit", async (func) => {
+    func.preventDefault();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    const res = await fetch(`${API_URL}?email=${email}&password=${password}`);
+    const users = await res.json();
+
+    if (users.length > 0) {
+        result.style.color = "green";
+        result.innerText = `Welcome back, ${users[0].name}! Redirecting...`;
+
+        setTimeout(() => {
+            window.location.href = "TheMan.html";
+        }, 1000);
+    }
+    else {
+        result.style.color = "red";
+        result.innerText = "Invaild email or password";
+    }
+});
